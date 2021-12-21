c38d8a8bc85256c0d777fa1766c4a6b7c9dd28fc
ShemiPeretz <shemi.p17@gmail.com>
2021-12-21 22:32:33 +0200

    work in progres - need to find a better splitting method

--- assignment2.py
+++ assignment2.py
@@ -6,6 +6,7 @@ import numpy as np
 import time
 import random
 import matplotlib
+import math
 from collections.abc import Iterable
 
 
@@ -15,8 +16,9 @@ class Assignment2:
         Here goes any one time calculation that need to be made before 
         solving the assignment for specific functions. 
         """
-        intersections_list = np.ndarray([10])
-
+        self.intersections_list = []
+        self.max_x = None
+        self.flag = True
 
     def intersections(self, f1: callable, f2: callable, a: float, b: float, maxerr=0.001) -> Iterable:
         """
@@ -53,33 +55,119 @@ class Assignment2:
         # replace this line with your solution
 
         diffrence_func = self.intersect_function(f1, f2)
-        self.newton_raphson(a, b, diffrence_func, 0.001)
+        split = np.array_split(range(a, b), abs(b - a)) # TODO: Change the spliting
+        right_bracket = b
 
-        X=[0]
-        return X
+        for i in range(len(split)):
+            a = split[i][0]
+            if i == len(split)-1:
+                b = right_bracket
+            else:
+                b = split[i+1][0]
+            if (diffrence_func(a) * diffrence_func(b)) >= 0:
+                guess = self.secant_guesser(a, b, diffrence_func, maxerr)
+            else:
+                guess = self.bisection_guesser(a, b, diffrence_func, maxerr)
+            if guess is None:
+                continue
+            if guess in self.intersections_list:
+                continue
+            self.newton_raphson(guess, a, b, diffrence_func, maxerr)
+
+        print(self.intersections_list)
+        return self.intersections_list
 
     def intersect_function(self, f1:callable, f2:callable):
         return lambda x: f1(x) - f2(x)
 
+    def bisection_guesser(self, left_bracket, right_bracket, function, maxerr):
+        # TODO: What if a and b arent legal? make it work!
+        new_err = maxerr*10
+        if (function(left_bracket) * function(right_bracket)) > 0:
+            return
+        if function(left_bracket) == 0:
+            return left_bracket
+        if function(right_bracket) == 0:
+            return right_bracket
+        c = left_bracket
+        self.max_x = (left_bracket + right_bracket) / 2
 
-    def newton_raphson(self, left_bracket, right_bracket, function, maxerr):
+        while (right_bracket - left_bracket) >= new_err:
+
+            # Find middle point
+            c = (left_bracket + right_bracket) / 2
+            if c > self.max_x:
+                self.max_x = c
+
+            # Check if middle point is root
+            if function(c) == 0.0:
+                break
+
+            # Decide the side to repeat the steps
+            if function(c) * function(left_bracket) < 0:
+                right_bracket = c
+            else:
+                left_bracket = c
+
+        if c > self.max_x:
+            self.max_x = c
+        return c
+
+    def secant_guesser(self, x1, x2, function, maxerr):
+        new_err = maxerr*10
+        n = 0
+        xm = 0
+        x0 = 0
+        c = 0
+        if function(x1) == 0:
+            return x1
+        if function(x2) == 0:
+            return x2
+        if function(x1) * function(x2) < 0:
+            while True:
+                # calculate the intermediate value
+                x0 = (x1 * function(x2) - x2 * function(x1)) / (function(x2) - function(x1))
+
+                # check if x0 is root of
+                # equation or not
+                c = function(x1) * function(x0)
+
+                # update the value of interval
+                x1 = x2
+                x2 = x0
+
+                # update number of iteration
+                n += 1
+
+                # if x0 is the root of equation
+                # then break the loop
+                if c == 0:
+                    break
+                xm = ((x1 * function(x2) - x2 * function(x1)) / (function(x2) - function(x1)))
+
+                if abs(xm - x0) < new_err:
+                    break
+
+            return x0
+
+        else:
+            # TODO: What do you do in that situation
+            return
+
+    def newton_raphson(self, guess,left_bracket, right_bracket, function, maxerr):
+        self.flag = False
         h = 0.00000000001
-        x = left_bracket
-        while x <= right_bracket:
-            deriv = (function(x + h) / h) - (function(x) / h)
+        x = guess
+        deriv = (function(x + h) / h) - (function(x) / h)
+        epsilon = function(x) / deriv
+        while abs(epsilon) >= maxerr:
             epsilon = function(x) / deriv
-            lst = []
-            while abs(epsilon) >= maxerr:
-                epsilon = function(x) / deriv
-                # x(i+1) = x(i) - f(x) / f'(x)
-                x = x - epsilon
-                deriv = (function(x + h) / h) - (function(x) / h)
+            # x(i+1) = x(i) - f(x) / f'(x)
+            x = x - epsilon
+            deriv = (function(x + h) / h) - (function(x) / h)
 
-            print("The value of the root is : ", "%.4f" % x)
-            # self.intersections_list.insert(x)
-            lst.append(x)
-            print(lst)
-            x += maxerr
+        # print("The value of the root is : ", "%.4f" % x)
+        self.intersections_list.append(x)
 
 
 ##########################################################################
@@ -152,14 +240,21 @@ class TestAssignment2(unittest.TestCase):
             self.assertGreaterEqual(0.001, abs(f1(x) - f2(x)))
 
     def test_3(self):
+        f3 =lambda x: 1.52*x**10 - 0.6189*x**9 + 1.58*x**8 - 0.01277*x**7 - 0.6578*x**6 + 0.3955*x**5 + 1.223*x**4 + \
+                      1.716*x**3 - 0.4981*x**2 + 0.3833*x - 1.359
+        f4 =lambda x: -0.2656*x**10+0.1583*x**9-0.1591*x**8+0.9435*x**7+1.406*x**6-0.4601*x**5-0.1547*x**4-0.5351*x**3 - \
+                      1.107*x**2+0.102*x +1.763
         f1 = lambda x: x**2
         f2 = lambda x: 2*x
 
-        a = 0
-        b = 2
+        f5 = lambda x: math.sin(10*x)
+        f6 = lambda x: math.cos(10*x)
+
+        a = -1
+        b = 1
 
         intersector = Assignment2()
-        intersector.intersections(f1, f2, a, b)
+        intersector.intersections(f5, f6, a, b)
 
 if __name__ == "__main__":
     unittest.main()
